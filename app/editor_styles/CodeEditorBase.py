85782de8f84cb6e1ed909414e8ddf45170552b1a
Brandon Fraune <brandon.fraune@gmail.com>
2025-04-10 09:45:04 -0500

    use monospaced font

--- app/editor_styles/CodeEditorBase.py
+++ app/editor_styles/CodeEditorBase.py
@@ -1,8 +1,12 @@
 from PySide6.QtWidgets import QPlainTextEdit
+from PySide6.QtGui import QFont
 
 
 class CodeEditorBase(QPlainTextEdit):
     def __init__(self, parent=None):
         super().__init__(parent)
+        monospace_font = QFont("Courier New")
+        monospace_font.setStyleHint(QFont.Monospace)
+        self.setFont(monospace_font)
         # Default weight parameter controlling "heaviness" of each line
         self.weight = 1.0
